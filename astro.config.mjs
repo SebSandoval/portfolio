24ac98beb833266f3899b7f7740170427174f721
Juan Sebastian Sandoval Vargas <sebastian@MacBook-Pro-de-Juan.local>
2026-04-04 21:32:54 -0500

    Fix asset paths for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://SebSandoval.github.io/portfolio",
+  base: "/portfolio",
   integrations: [react()],
 
   vite: {
