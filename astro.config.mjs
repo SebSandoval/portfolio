642b7dd3af7f43d268f2783f945e98aeaf0b2641
Juan Sebastian Sandoval Vargas <sebastian@MacBook-Pro-de-Juan.local>
2026-04-04 21:30:05 -0500

    Add GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://SebSandoval.github.io/portfolio",
   integrations: [react()],
 
   vite: {
